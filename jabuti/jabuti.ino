3e611e80ea8418644df6a335f56935c7aabd42df
Giulia Franca <82042116+giuliafranca@users.noreply.github.com>
2022-10-11 16:33:57 -0300

    codigo mais certo que errado
    necessidade de entender e melhorar o funcionamento do encoder, que está dando errado.

--- jabuti/jabuti.ino
+++ jabuti/jabuti.ino
@@ -1,18 +1,18 @@
-#define SensorDrt 4
-#define SensorEsq 8
-#define SensorFrente1 7
-#define SensorFrente2 5
+#define SensorDrt 7
+#define SensorEsq 5
+#define SensorFrente1 8
+#define SensorFrente2 4
 #define Encoder1 2
 #define Encoder2 3
 
-#define MotorE_sentido1 11
-#define MotorE_sentido2 10
-#define MotorD_sentido1 9
-#define MotorD_sentido2 6
+#define MotorD_sentido1 11
+#define MotorD_sentido2 10
+#define MotorE_sentido1 9
+#define MotorE_sentido2 6
 
 unsigned int pulsos_por_volta = 20;
-int pwm, voltas1, voltas2;
-unsigned long int pulsos1, pulsos2;
+int pwm;
+volatile long int pulsos1;
 
 void setup() {
   Serial.begin(9600);
@@ -26,11 +26,9 @@ void setup() {
   pinMode(SensorFrente1, INPUT);
   pinMode(SensorFrente2, INPUT);
   pinMode(Encoder1, INPUT);
-  pinMode(Encoder2, INPUT);
 
   //aciona o contador a cada pulso
-  attachInterrupt(Encoder1, Contador1, RISING); 
-  attachInterrupt(Encoder2, Contador2, RISING); 
+  attachInterrupt(digitalPinToInterrupt(Encoder1), Contador1, FALLING); 
 
 /* boas práticas*/
   analogWrite(MotorE_sentido1, 0);
@@ -40,52 +38,50 @@ void setup() {
 
   pwm = 0;
   pulsos1 = 0;
-  voltas1 = 0;
-  pulsos2 = 0;  
-  voltas2 = 0;
 }
 
 void loop() {
-  Correr(64);
-  if(digitalRead(SensorDrt) == 1){ //direita liberada
-    Parar();
-    RodarDir(64, 64, 6);
-  }else if (digitalRead(SensorEsq) == 1 && (digitalRead(SensorFrente1) || digitalRead(SensorFrente2)) == 0 && digitalRead(SensorDrt) == 0){ //apenas esquerda liberada
-    Parar();
-    RodarEsq(64, 64, 6);
-  }else if((digitalRead(SensorFrente1) || digitalRead(SensorFrente2)) == 0 && digitalRead(SensorEsq) == 0 && digitalRead(SensorDrt) == 0){ //todos bloqueados
-    Parar();
-    RodarDir(64, 64, 12);
-  }
+//  Correr(128);
+//  if(digitalRead(SensorDrt) == 1){ //direita liberada
+//    Parar();
+//    RodarDir(64, 3);
+//  }else if (digitalRead(SensorEsq) == 1 && (digitalRead(SensorFrente1) || digitalRead(SensorFrente2)) == 0 && digitalRead(SensorDrt) == 0){ //apenas esquerda liberada
+//    Parar();
+//    RodarEsq(64, 6);
+//  }else if((digitalRead(SensorFrente1) || digitalRead(SensorFrente2)) == 0 && digitalRead(SensorEsq) == 0 && digitalRead(SensorDrt) == 0){ //todos bloqueados
+//    Parar();
+//    RodarDir(64, 12);
+//  }
+ //   RodarEsq(100, 20);
+    Parar();    
 }
 
 
 void Correr(int pwm){
-  analogWrite(MotorE_sentido1, 0);
-  analogWrite(MotorE_sentido2, pwm);
-  analogWrite(MotorD_sentido1, 0);
-  analogWrite(MotorD_sentido2, pwm);
+  analogWrite(MotorE_sentido1, pwm);
+  analogWrite(MotorE_sentido2, 0);
+  analogWrite(MotorD_sentido1, pwm);
+  analogWrite(MotorD_sentido2, 0);
 }
 
-void RodarDir(int pwm1,int pwm2, int vlt){
+void RodarDir(int pwm, int psl){
   pulsos1 = 0;
-  voltas1 = 0;
-  while(voltas1 < vlt){
-    analogWrite(MotorE_sentido1, pwm1);
+  while(pulsos1 <= psl){
+    analogWrite(MotorE_sentido1, pwm);
     analogWrite(MotorE_sentido2, 0);
     analogWrite(MotorD_sentido1, 0);
-    analogWrite(MotorD_sentido2, pwm2);
+    analogWrite(MotorD_sentido2, pwm);
   }
 }
 
-void RodarEsq(int pwm1,int pwm2, int vlt){
+void RodarEsq(int pwm, int psl){
   pulsos1 = 0;
-  voltas1 = 0;
-  while(voltas1 < vlt){
+  while(pulsos1 <= psl){
     analogWrite(MotorE_sentido1, 0);
-    analogWrite(MotorE_sentido2, pwm1);
-    analogWrite(MotorD_sentido1, pwm2);
+    analogWrite(MotorE_sentido2, pwm);
+    analogWrite(MotorD_sentido1, pwm);
     analogWrite(MotorD_sentido2, 0);
+//    Serial.println(voltas1);
   }
 }
 
@@ -94,14 +90,11 @@ void Parar(){
   digitalWrite(MotorE_sentido2, HIGH);
   digitalWrite(MotorD_sentido1, HIGH);
   digitalWrite(MotorD_sentido2, HIGH); 
+  delay(4000);
 }
 
 void Contador1(){ 
   pulsos1++;
-  voltas1 = pulsos1/20; 
-}
-
-void Contador2(){
-  pulsos2++;
-  voltas2 = pulsos2/20;
+  delay(100);
+  Serial.println(pulsos1);
 }
